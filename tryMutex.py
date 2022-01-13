1a0feb7da1c59a3b563a0730841f809011b6a864
Jessica13 <jessica.spera@insa-lyon.fr>
2022-01-13 02:38:56 +0100

    add def + images + musique

--- tryMutex.py
+++ tryMutex.py
@@ -35,3 +35,46 @@ def run(n):
 if __name__ == '__main__':
     
     run(10)
+
+
+'''
+import threading
+numOfGames = 10
+resultList = [[] for x in xrange(numOfGames)]
+
+class Game(threading.Thread):
+
+    def __init__(self, player1, player2, number):
+        threading.Thread.__init__(self)
+        self.number = number
+        self.player1 = player1
+        self.player2 = player2
+
+    def run(self):
+
+        #Your game algorithm between self.player1 and self.player2 is here
+
+        #Put game result into shared list
+        resultList[self.number] = <result>
+
+if __name__ == '__main__':
+    #Create instances of the Game() class, pass your Player() objects
+    #You can do the step in loop
+    game0 = Game(<PlayerObj>,<PlayerObj>,0)
+    game1 = Game(<PlayerObj>,<PlayerObj>,1)
+
+    #Start execution of Game.run() methods of all Game() objects in separate threads
+    #You can do the step in loop
+    game0.start()
+    game1.start()
+
+    #Wait until all Game.run() methods are executed = all threads are finished
+    #You can do the step in loop
+    game0.join()
+    game1.join()
+
+    #In the shared list you have corresponding return values, 
+    #whatever they are in your case [[result of Game 0],[result of Game 1],[]...]
+    print resultList
+
+    '''
